9720e2a11ab4f72df5bf41d4e3f2c39a92bc18b2
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-03-18 18:04:59 +0900

    inner join 내에 서브 쿼리를 querydsl이 지원하지 않는다... 그래서 의존성 롤백함. native query를 활용예정.

--- src/main/java/com/dasd412/domain/diary/DiabetesDiaryRepository.java
+++ src/main/java/com/dasd412/domain/diary/DiabetesDiaryRepository.java
@@ -1,44 +1,16 @@
 package com.dasd412.domain.diary;
 
 import java.time.LocalDateTime;
-import java.util.Set;
 import java.util.List;
 
-import com.dasd412.domain.diet.HashTag;
-import com.querydsl.core.types.ExpressionUtils;
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public interface DiabetesDiaryRepository extends JpaRepository<DiabetesDiary,Long> {
 
     @Query(value = "from DiabetesDiary dd where dd.writtenTime BETWEEN :startDate AND :endDate")
      List<DiabetesDiary> findAllBetweenDates(@Param("startDate")LocalDateTime startDate, @Param("endDate")LocalDateTime endDate);
 
-    default List<Object[]>findMonthlyAverage(LocalDateTime startDate, LocalDateTime endDate){
-        QDiabetesDiary diabetesDiary=QDiabetesDiary.diabetesDiary;
-
-        EntityManagerFactory emf= Persistence.createEntityManagerFactory("manager");
-        EntityManager em=emf.createEntityManager();
-
-        JPAQuery query=new JPAQuery(em);
-
-        List<Object[]>found=query
-                .select(diabetesDiary.writtenTime.month(),
-                        (diabetesDiary.fastingPlasmaGlucose).add(diabetesDiary.breakfastBloodSugar).add(diabetesDiary.lunchBloodSugar).add(diabetesDiary.dinnerBloodSugar).divide(4.0).divide(diabetesDiary.writtenTime.month()) )
-                .innerJoin()
-                .on()
-                .where(diabetesDiary.writtenTime.between(startDate,endDate))
-                .groupBy(diabetesDiary.writtenTime.month());
-        em.close();
-        emf.close();
-        return found;
-    }
+    //List<Object[]>findMonthlyAverage(LocalDateTime startDate, LocalDateTime endDate);
 }
