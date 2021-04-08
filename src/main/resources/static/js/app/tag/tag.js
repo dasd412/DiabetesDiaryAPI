77a8f879308babe1dcf2cb7a89f13066ac3a641d
dasd412 <45190017+dasd412@users.noreply.github.com>
2021-04-08 09:48:14 +0900

    페이지 이동 처리 작성완료.

--- src/main/resources/static/js/app/tag/tag.js
+++ src/main/resources/static/js/app/tag/tag.js
@@ -1,5 +1,18 @@
 
 $(document).ready(function(){
+ makePageList();
+
+ const pageForm=$("#pageForm");
+
+ $(".pagination a").click(function(e){
+  e.preventDefault();
+  pageForm.find('[name="page"]').val($(this).attr('href'));
+  pageForm.submit();
+ });
+
+});
+
+function makePageList(){
  let pageLength=$(".pagination").children('li').length;
  let start=0;
  let end=pageLength;
@@ -8,6 +21,7 @@ $(document).ready(function(){
  if(preId!=undefined){
    $("#previousPage").children('a').text("PREV "+(parseInt(preId)+1));
    $("#previousPage").children('a').css("color","black");
+   $("#previousPage").children('a').attr("href",(parseInt(preId)+1));
    start++;
  }
 
@@ -15,19 +29,19 @@ $(document).ready(function(){
  if(nextId!=undefined){
   $("#nextPage").children('a').text("NEXT "+(parseInt(nextId)+1));
   $("#nextPage").children('a').css("color","black");
+  $("#nextPage").children('a').attr("href",(parseInt(nextId)+1));
   end--;
  }
-
- console.log(start);
- console.log(end);
 
  let currentPage=$("#currentPage").val();
 
  for(let i=start;i<end;i++){
   const li=$(".pagination").children('li').eq(i);
-  console.log(li);
   const a=li.children('a');
+
   a.text(parseInt(li.attr("id"))+1);
+  a.attr("href",parseInt(li.attr("id"))+1);
+
   if(li.attr("id")==currentPage-1){
    li.attr('class','page-item active');
   }
@@ -35,4 +49,4 @@ $(document).ready(function(){
    li.attr('class','page-item');
   }
  }
-});
+}
